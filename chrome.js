40cd6bb1e26a5be11822e870260c168d6e2e174d
Alex Smith <alex.smith2844@gmail.com>
2020-10-09 21:04:15 +0500

    update

--- chrome.js
+++
@@ -1,6 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch({ executablePath: '/usr/bin/chromium' });
-  await browser.close();
-})();
--- examples/systeminfo/app.js
+++ examples/systeminfo/app.js
@@ -1,65 +1,65 @@
-/**
- * Copyright 2018 Google Inc., PhantomJS Authors All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#!/usr/bin/env node
 
-'use strict';
+const carlo = require('carlo');
+const { rpc } = require('carlo/rpc');
 
-const carlo = require(
-	'../../'
-	// 'carlo'
-);
 const os = require('os');
 const path = require('path');
 const si = require('systeminformation');
 
-async function run() {
-  let app;
-  try {
-    app = await carlo.launch(
-        {
-          bgcolor: '#2b2e3b',
-          title: 'Systeminfo App',
-          width: 1000,
-          height: 500,
-          channel: ['canary', 'stable', 'chromium'],
-          icon: path.join(__dirname, '/app_icon.png'),
-          args: process.env.DEV === 'true' ? ['--auto-open-devtools-for-tabs'] : [],
-          localDataDir: path.join(os.homedir(), '.carlosysteminfo'),
-        });
-  } catch(e) {
-    // New window is opened in the running instance.
-    console.log('Reusing the running instance', e);
-    return;
-  }
-  app.on('exit', () => process.exit());
-  // New windows are opened when this app is started again from command line.
-  app.on('window', window => window.load('index.html'));
-  app.serveFolder(path.join(__dirname, 'www'));
-  await app.exposeFunction('systeminfo', systeminfo);
-  await app.load('index.html');
-  return app;
+class Backend {
+	constructor(app) {
+		this.app_ = app;
+		this.windows_ = new Map();
+	}
+	showMyWindow(url) {
+		let windowPromise = this.windows_.get(url);
+		if (!windowPromise) {
+			windowPromise = this.createWindow_(url);
+			this.windows_.set(url, windowPromise);
+		}
+		windowPromise.then(w => w.bringToFront());
+	}
+	async createWindow_(url) {
+		const window = await this.app_.createWindow({
+			width: 800, height: 600, top: 200, left: 10
+		});
+		window.on('close', () => this.windows_.delete(url));
+		window.load(url);
+		return window;
+	}
 }
-
 async function systeminfo() {
-  const info = {};
-  await Promise.all([
-    si.battery().then(r => info.battery = r),
-    si.cpu().then(r => info.cpu = r),
-    si.osInfo().then(r => info.osInfo = r),
-  ]);
-  return info;
+	const info = {};
+	await Promise.all([
+		si.battery().then(r => info.battery = r),
+		si.cpu().then(r => info.cpu = r),
+		si.osInfo().then(r => info.osInfo = r),
+	]);
+	return info;
 }
 
-run();
+(async() => {
+	let app;
+	try {
+		app = await carlo.launch({
+			bgcolor: '#2b2e3b',
+			title: 'Systeminfo App',
+			width: 1000,
+			height: 600,
+			// channel: ['canary', 'stable', 'chromium'],
+			icon: path.join(__dirname, '/icon.png'),
+			localDataDir: path.join(os.homedir(), '.carlosysteminfo'),
+		});
+	} catch(e) {
+		console.log('Reusing the running instance', e);
+		return;
+	}
+	app.on('exit', () => process.exit());
+	const mainWindow = app.mainWindow();
+	mainWindow.on('close', () => process.exit());
+	mainWindow.serveFolder(__dirname);
+	await mainWindow.exposeFunction('systeminfo', systeminfo);
+	await mainWindow.load('index.html', rpc.handle(new Backend(app)));
+	return app;
+})();
